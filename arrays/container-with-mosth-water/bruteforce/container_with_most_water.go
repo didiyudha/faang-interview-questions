bd6d92a4b54b36a110df0f6cb94ece3d87836a01
didiyudha <didiyudha@gmail.com>
2020-11-10 14:58:15 +0700

    add container with most water case

---
+++ arrays/container-with-mosth-water/bruteforce/container_with_most_water.go
@@ -0,0 +1,30 @@
+package container_with_mosth_water
+
+func MaxArea(height []int) int {
+	var maxArea int
+	for p1 := 0; p1 < len(height); p1++ {
+		for p2 := p1+1; p2 < len(height); p2++ {
+			height := findHeight(height[p1], height[p2])
+			width := p2 - p1
+			area := findMaxArea(height, width)
+			if maxArea < area {
+				maxArea = area
+			}
+		}
+	}
+	return maxArea
+}
+
+func findHeight(h1, h2 int) int {
+	if h1 == h2 {
+		return h1
+	}
+	if h1 < h2 {
+		return h1
+	}
+	return h2
+}
+
+func findMaxArea(height, width int) int {
+	return height * width
+}

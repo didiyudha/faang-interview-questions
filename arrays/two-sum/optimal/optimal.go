309f09d6d71404ddababe157537b7b8561b1747d
didiyudha <didiyudha@gmail.com>
2020-11-10 12:43:46 +0700

    add two sum optimal function

--- arrays/two-sum/optimal/optimal.go
+++ arrays/two-sum/optimal/optimal.go
@@ -1,1 +1,14 @@
 package optimal
+
+func TwoSum(nums []int, target int) []int {
+	numMaps := make(map[int]int)
+	for p, n := range nums{
+		idx, ok := numMaps[n]
+		if ok {
+			return []int{idx, p}
+		}
+		numToFind := target - n
+		numMaps[numToFind] = p
+	}
+	return nil
+}

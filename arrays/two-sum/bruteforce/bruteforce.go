89102964a0423531a2e1bc6731f9a8cab594656d
didiyudha <didiyudha@gmail.com>
2020-11-10 12:42:51 +0700

    add two sum bruteforce solustions

--- arrays/two-sum/bruteforce/bruteforce.go
+++ arrays/two-sum/bruteforce/bruteforce.go
@@ -1,1 +1,13 @@
 package bruteforce
+
+func TwoSum(nums []int, target int) []int {
+	for p1 := 0; p1 < len(nums); p1++ {
+		numToFind := target - nums[p1]
+		for p2 := p1 + 1; p2 < len(nums); p2++ {
+			if nums[p2] == numToFind {
+				return []int{p1, p2}
+			}
+		}
+	}
+	return nil
+}

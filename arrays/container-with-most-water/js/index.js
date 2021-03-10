10d9880e9b57ac243b747cfb9be3836fdc1c9038
didiyudha <didiyudha@gmail.com>
2021-03-10 11:09:40 +0700

    optimal solutions for max area in js

--- arrays/container-with-most-water/js/index.js
+++ arrays/container-with-most-water/js/index.js
@@ -3,19 +3,38 @@ function maxArea(arr) {
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = i+1; j < arr.length; j++) {
-      
       const height = Math.min(arr[i], arr[j]);
       const width = j - i;
       const area = countArea(height, width);
-      
-      if (max < area) {
-        max = area;
-      }
-
+      max = Math.max(max, area);
     }
   }
 
   return max;
+}
+
+function optimalMaxArea(arr) {
+  
+  let max = 0;
+  let p1 = 0;
+  let p2 = arr.length-1;
+
+  while(p1 < p2) {
+    const height = Math.min(arr[p1], arr[p2]);
+    const width = p2 - p1;
+    const area = countArea(height, width);
+    max = Math.max(max, area);
+
+    if (arr[p1] < arr[p2]) {
+      p1++;
+    } else {
+      p2--;
+    }
+
+  }
+
+  return max;
+
 }
 
 function countArea(height, width) {
@@ -24,4 +43,5 @@ function countArea(height, width) {
 
 module.exports = {
   maxArea,
+  optimalMaxArea,
 };
--- arrays/container-with-most-water/js/test.js
+++ arrays/container-with-most-water/js/test.js
@@ -1,19 +1,41 @@
-const { maxArea } = require('./index');
+const { maxArea, optimalMaxArea } = require('./index');
 
-test('should return 32', () => {
-  const heightsArray = [4,8,1,2,3,9];
-  const max = maxArea(heightsArray);
-  expect(max).toEqual(32);
+describe('Bruteforce', () => {
+  test('should return 32', () => {
+    const heightsArray = [4,8,1,2,3,9];
+    const max = maxArea(heightsArray);
+    expect(max).toEqual(32);
+  });
+  
+  test('should return 28', () => {
+    const heightsArray = [7, 1, 2, 3, 9];
+    const max = maxArea(heightsArray);
+    expect(max).toEqual(28);
+  });
+  
+  test('should return 15', () => {
+    const heightsArray = [5, 2, 8, 7];
+    const max = maxArea(heightsArray);
+    expect(max).toEqual(15);
+  });
 });
 
-test('should return 28', () => {
-  const heightsArray = [7, 1, 2, 3, 9];
-  const max = maxArea(heightsArray);
-  expect(max).toEqual(28);
+describe('Optimal solution', () => {
+  test('should return 32', () => {
+    const heightsArray = [4,8,1,2,3,9];
+    const max = optimalMaxArea(heightsArray);
+    expect(max).toEqual(32);
+  });
+  
+  test('should return 28', () => {
+    const heightsArray = [7, 1, 2, 3, 9];
+    const max = optimalMaxArea(heightsArray);
+    expect(max).toEqual(28);
+  });
+  
+  test('should return 15', () => {
+    const heightsArray = [5, 2, 8, 7];
+    const max = optimalMaxArea(heightsArray);
+    expect(max).toEqual(15);
+  });
 });
-
-test('should return 15', () => {
-  const heightsArray = [5, 2, 8, 7];
-  const max = maxArea(heightsArray);
-  expect(max).toEqual(15);
-})

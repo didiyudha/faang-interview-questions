eff418802c11455b84b49f3f4cc7c8c1bbdb4c2d
didiyudha <didiyudha@gmail.com>
2021-03-12 18:42:52 +0700

    compare backspace problem optimal solution in js

--- strings/typed_out_string/js/test.js
+++ strings/typed_out_string/js/test.js
@@ -1,6 +1,6 @@
-const { compareBackspaceString } = require('./index');
+const { compareBackspaceString, compareBackspaceOptimal } = require('./index');
 
-describe('Bruteforce', () => {
+describe('Bruteforce solution', () => {
 
   test('should return true with string ac', () => {
     const S = "ab#c";
@@ -30,4 +30,34 @@ describe('Bruteforce', () => {
     expect(isEqual).toEqual(false);
   });
 
+});
+
+describe('Optimal soulution', () => {
+  test('should return true with string ac', () => {
+    const S = "ab#c";
+    const T = "ad#c";
+    const isEqual = compareBackspaceOptimal(S, T);
+    expect(isEqual).toEqual(true);
+  })
+
+  test('should return true with empty string', () => {
+    const S = "ab##";
+    const T = "c#d#";
+    const isEqual = compareBackspaceOptimal(S, T);
+    expect(isEqual).toEqual(true);
+  });
+
+  test('should return true with string c', () => {
+    const S = "a##c";
+    const T = "#a#c";
+    const isEqual = compareBackspaceOptimal(S, T);
+    expect(isEqual).toEqual(true);
+  });
+
+  test('should return false', () => {
+    const S = "a#c";
+    const T = "b";
+    const isEqual = compareBackspaceOptimal(S, T);
+    expect(isEqual).toEqual(false);
+  });
 });
